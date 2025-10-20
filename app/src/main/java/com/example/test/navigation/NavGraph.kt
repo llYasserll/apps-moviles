60090674539f5e1f227896e6002163e633451d5f
Eddy Chanca 1309 <eddy_13_mj@hotmail.com>
2025-10-20 16:52:29 -0500

    Avance Eddy Chanca
    Login screen
    ForgotPassword Screen
    Register screen terminados y conectado al login

---
+++ app/src/main/java/com/example/test/navigation/NavGraph.kt
@@ -0,0 +1,34 @@
+package com.example.test.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.test.ui.screens.SplashScreen
+import com.example.test.ui.screens.LoginScreen
+import com.example.test.ui.screens.RegisterScreen
+import com.example.test.ui.screens.ForgotPasswordScreen
+import com.example.test.ui.onboarding.screens.OnboardingScreen
+
+@Composable
+fun NavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        //composable("splash") { SplashScreen(navController) }
+        composable("login") { LoginScreen(navController) }
+        composable("forgot_password") {
+            ForgotPasswordScreen(navController)
+        }
+        composable("register") {
+            RegisterScreen(navController)
+        }
+
+        composable("onboarding") {
+            OnboardingScreen(
+                onNext = { navController.navigate("login") }
+            )
+        }
+    }
+}
