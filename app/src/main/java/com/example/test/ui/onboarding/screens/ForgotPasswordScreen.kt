04e51179b3b318ee79a467046c64ae6168be17f7
Rhymes003 <n00466264@upn.pe>
2025-11-12 17:10:27 -0500

    fix

--- app/src/main/java/com/example/test/ui/onboarding/screens/ForgotPasswordScreen.kt
+++ app/src/main/java/com/example/test/ui/onboarding/screens/ForgotPasswordScreen.kt
@@ -9,6 +9,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -120,3 +123,11 @@ fun ForgotPasswordScreen(navController: NavHostController) {
         }
     }
 }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ForgotPasswordScreenPreview() {
+    val navController = rememberNavController()
+    ForgotPasswordScreen(navController)
+}
+
--- app/src/main/java/com/example/test/ui/onboarding/screens/TrackingScreen.kt
+++ app/src/main/java/com/example/test/ui/onboarding/screens/TrackingScreen.kt
@@ -12,10 +12,10 @@ import com.google.android.gms.maps.model.LatLng
 fun TrackingScreen(cafeName: String) {
 
     val cafes = listOf(
-        CafeItem("Café Espresso", "S/ 8.00", R.drawable.cafe_espresso, -12.0464, -77.0428),
-        CafeItem("Café Americano", "S/ 9.00", R.drawable.cafe_americano, -12.0564, -77.0500),
-        CafeItem("Café Latte", "S/ 10.00", R.drawable.cafe_latte, -12.0600, -77.0700),
-        CafeItem("Café Capuccino", "S/ 11.00", R.drawable.cafe_capuccino, -12.0620, -77.0800)
+        CafeItem("Café Espresso", "S/ 8.00", R.drawable.cafe_espresso, -15.838836890992736, -70.02814384810597),
+        CafeItem("Café Americano", "S/ 9.00", R.drawable.cafe_americano, -12.005303051009932, -77.09893414659031),
+        CafeItem("Café Latte", "S/ 10.00", R.drawable.cafe_latte, -13.517684809887477, -71.97473092698121),
+        CafeItem("Café Capuccino", "S/ 11.00", R.drawable.cafe_capuccino, -12.076499720223184, -77.03668521687571)
     )
 
     val cafe = cafes.find { it.nombre == cafeName } ?: cafes.first()
